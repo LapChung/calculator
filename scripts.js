beb3f671026c118c8e09ed8a3df1c81ad64e5253
Lap Chung <lapchung12@gmail.com>
2022-08-04 19:25:05 -0400

    layout of calculator

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll("[data-number]");
+const operatorButtons = document.querySelectorAll("[data-operator");
+const equalsButton = document.querySelector("[data-equals]");
+const deleteButton = document.querySelector("[data-delete]");
+const allClearButton = document.querySelector("[data-all-clear]");
+const prevOperator = document.querySelector("[data-prev-operator]");
+const currOperator = document.querySelector("[data-curr-operator]");
